0f49527cdbc4822c107c9870a74161c22a1e0e94
crazydumbbell <wogus5357@gmail.com>
2023-11-29 16:21:53 +0900

    complete

--- src/pages/day.jsx
+++ src/pages/day.jsx
@@ -2,13 +2,15 @@ import { useParams } from "react-router-dom";
 import englishData from "../englishData.json";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-englishData.map((v) => console.log(v));
+console.log(process.env.REACT_APP_API_KEY);
 
 const Day = () => {
   const [dailyData, setDailyData] = useState();
   const [isVisible, setIsVisible] = useState(false);
   const [currentPage, setCurrentPage] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
 
   const { day } = useParams();
 
@@ -25,6 +27,60 @@ const Day = () => {
     setCurrentPage((prevPage) => prevPage - 1);
   };
 
+  const onClickSound = async () => {
+    try {
+      setIsLoading(true);
+
+      if (isLoading) return;
+      // setIsLoading만 만들고 끝이아니라 여기서 if를 걸어줘야됨
+
+      const response = await axios.post(
+        `https://texttospeech.googleapis.com/v1/text:synthesize?key=${process.env.REACT_APP_API_KEY}`,
+        {
+          input: {
+            text: dailyData.sentences[currentPage].english,
+          },
+          // 읽어줄 텍스트파일
+          voice: {
+            languageCode: "en-gb",
+            name: "en-GB-Standard-A",
+            ssmlGender: "FEMALE",
+          },
+          audioConfig: {
+            audioEncoding: "MP3",
+            speakingRate: 1,
+            pitch: 5,
+          },
+        }
+      );
+
+      console.log(response);
+
+      // 받은파일은 디코딩(atob함수 쓰면됨)
+      const binaryData = atob(response.data.audioContent);
+      // //////////////
+
+      const byteArray = new Uint8Array(binaryData.length);
+
+      for (let i = 0; i < binaryData.length; i++) {
+        byteArray[i] = binaryData.charCodeAt(i);
+      }
+      // charCodeAt은 byte 단위로 자르면서 변환까지 시켜주는 함수
+      // 자른 형태를 Blob이라는 배열에 담자
+      const blob = new Blob([byteArray.buffer], { type: "audio/mp3 " });
+
+      const newAudio = new Audio(URL.createObjectURL(blob));
+      console.log(newAudio);
+
+      document.body.appendChild(newAudio);
+      newAudio.play();
+
+      setTimeout(() => setIsLoading(false), 3000);
+    } catch (error) {
+      console.error(error);
+      setIsLoading(false);
+    }
+  };
   // ////////////////////
   useEffect(() => {
     englishData.map((v) => {
@@ -72,7 +128,9 @@ const Day = () => {
           <button className="btn-style " onClick={onClickNext}>
             Next
           </button>
-          <button className="btn-style">Sound</button>
+          <button className="btn-style" onClick={onClickSound}>
+            Sound
+          </button>
         </div>
       </div>
     </div>
